85d2b21f3b3fa2ed21e822075fe431a745dcf3b0
unknown <Student@LIB-UGEXT4-012.wf.uct.ac.za>
2024-08-16 11:59:50 +0200

    Added dataSource, and connexion to my data objects(tables)

--- WaiteringSystem/Properties/Settings.Designer.cs
+++ WaiteringSystem/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace WaiteringSystem.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\EmployeeDatab" +
+            "ase.mdf;Integrated Security=True")]
+        public string EmpDbConStr {
+            get {
+                return ((string)(this["EmpDbConStr"]));
+            }
+        }
     }
 }
